c329a3958b6deb3d3675dbea9ab8df93e4c0b0dc
Wali Hasan <wali_hasan@brown.edu>
2026-06-07 22:46:01 -0400

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> valMap{};
+        
+        for (int i{}; i<nums.size(); i++) {
+            int diff = target - nums[i];
+            if (valMap.contains(diff)) {
+                return {valMap[diff], i};
+            }
+            valMap[nums[i]] = i;
+        }
+
+
+    }
+};
