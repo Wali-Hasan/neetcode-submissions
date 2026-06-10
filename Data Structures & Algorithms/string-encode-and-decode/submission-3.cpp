b502d2fc4e476b898a5503305e623e1352a85300
Wali Hasan <wali_hasan@brown.edu>
2026-06-10 12:19:04 -0400

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string res = "";
+        for (const auto& s: strs) {
+            res.append(std::to_string(s.length())).append("#").append(s);
+        }
+        return res; 
+    }
+
+    vector<string> decode(string s) {
+        vector<string> res{};
+
+        int i{0};
+        int j{i};
+        while (i < s.length()) {
+            j=i;
+            while (s[j] != '#') {
+                ++j;
+            }
+            int len{stoi(s.substr(i, j-i))};
+            ++j;
+            res.push_back(s.substr(j, len));
+            i = j + len;
+
+        }
+        return res;
+
+    }
+    // 3#wor2#it
+};
