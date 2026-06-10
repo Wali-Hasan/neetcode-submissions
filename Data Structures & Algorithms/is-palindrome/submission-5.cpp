89fc50e9b223539a647746186bdc223908c5f70f
Wali Hasan <wali_hasan@brown.edu>
2026-06-10 13:24:52 -0400

    Add: is-palindrome - submission-5

---
+++ Data Structures & Algorithms/is-palindrome/submission-5.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        size_t left{0};
+        size_t right{s.size()-1};
+        while (left < right) {
+            if (!isAlnum(s[right])) {
+                right--;
+                continue;
+            }
+            if (!isAlnum(s[left])) {
+                left++;
+                continue;
+            }
+            if (tolower(s[left]) != tolower(s[right])) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true; 
+    }
+
+    bool isAlnum(char c) {
+        bool isAl = ((tolower(c) - 'a') < 26) && ((tolower(c) - 'a') >= 0);
+        bool isNum = (c - '0') <= 9 && (c-'0') >=0;
+
+        return isAl || isNum;
+    }
+};
