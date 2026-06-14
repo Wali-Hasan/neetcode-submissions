fea546a7a5b61a1ffdcf757cf34f898682973da1
Wali Hasan <wali_hasan@brown.edu>
2026-06-14 11:54:35 -0400

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        return binarySearch(nums, target, 0, nums.size()-1);
+        
+    }
+
+    int binarySearch(vector<int>& nums, int target, int left, int right) {
+        if (left > right) return -1;
+        int mid = (left + right)/2;
+
+        if (nums[mid] == target) return mid;
+
+        if (target < nums[mid]) {
+            return binarySearch(nums, target, left, mid - 1);
+        } else {
+            return binarySearch(nums, target, mid + 1, right);
+        }
+    }
+};
