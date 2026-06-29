bd44c3f499a01cde486de90fe51b5f342b1e7286
Wali Hasan <wali_hasan@brown.edu>
2026-06-29 11:19:06 -0400

    Add: remove-duplicates-from-sorted-array - submission-5

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array/submission-5.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int l{1};
+
+        for (int r{1}; r < nums.size(); r++) {
+            if (nums[r] == nums[r-1]) {
+                continue; 
+            }
+            nums[l] = nums[r];
+            l++;
+        }
+        return l; 
+
+        
+
+        
+
+
+    }
+
+};
