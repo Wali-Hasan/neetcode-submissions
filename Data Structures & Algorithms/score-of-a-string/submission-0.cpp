ac3facc717d1565e397541c3df4e38c8bd796b01
Wali Hasan <wali_hasan@brown.edu>
2026-06-30 12:56:05 -0400

    Add: score-of-a-string - submission-0

---
+++ Data Structures & Algorithms/score-of-a-string/submission-0.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int res{};
+
+        for (int i{1}; i < s.length(); i++) {
+            res+=abs(s[i]-s[i-1]);
+        }
+        return res; 
+    }
+};
